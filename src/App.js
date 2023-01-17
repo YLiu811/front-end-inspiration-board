19b9ffd337192fee68a5d2021e6d70679d0cc9f2
Samantha Hunt <ariaandsam@gmail.com>
2023-01-17 11:38:17 -0800

    kate and sam created board component to use cardlist to render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import CardList from "./components/CardList";
-
+import Board from "./components/Board";
+import {useState} from 'react'
 const CARD_LIST = [
   {
     card_id: 1,
@@ -13,16 +14,24 @@ const CARD_LIST = [
     owner: "Megan",
   },
 ];
+const test_board = {
+    board_id: 1,
+    title: "Demo",
+    owner: "Sam",
+    cards: CARD_LIST
+}
 
 function App() {
   console.log("App component is rendering");
-
+  
+  console.log(test_board)
   return (
     <div>
       <header></header>
       <main>
         <h1>Inspiration Board</h1>
-        <CardList cards={CARD_LIST} />
+        <Board id={test_board.board_id} title={test_board.title} owner={test_board.owner} cards={test_board.cards} />
+        {/* <CardList cards={CARD_LIST} /> */}
       </main>
     </div>
   );
